16e4d331f236a9a6e0ada435b6a4cdf5f0107cd4
VitorGuilherme <alfredo.neg9@gmail.com>
2022-10-24 20:51:31 -0300

    almost done

--- angular-auth-front/src/app/core/components/auth/auth.module.ts
+++ angular-auth-front/src/app/core/components/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+//Routing
 import { AuthRoutingModule } from './auth-routing.module';
+
+//Pages
 import { SignComponent } from './pages/sign/sign.component';
 
 
@@ -11,7 +15,8 @@ import { SignComponent } from './pages/sign/sign.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
